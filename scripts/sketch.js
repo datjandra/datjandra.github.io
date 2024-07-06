b8a599f934b79dbb8fa162d68ed74e7248651b32
datjandra <donny.tjandra@gmail.com>
2024-07-06 09:51:13 -1000

    Update sketch.js

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -1,13 +1,11 @@
-let bodySegmentation;
 let video;
-let segmentation;
-let options = {
-  maskType: "parts",
-};
+let bodyPose;
+let poses = [];
+let connections;
 let selectedExercise = 'squat';
 
 function preload() {
-  bodySegmentation = ml5.bodySegmentation("BodyPix", options);
+  bodyPose = ml5.bodyPose('BlazePose');
 }
 
 function setup() {
@@ -21,8 +19,9 @@ function setup() {
   select('#exerciseDropdown').changed(selectExercise);
 }
 
-function startDetect() {
-  bodySegmentation.detectStart(video, gotResults);
+function startDetect() {  
+  bodyPose.detectStart(video, gotPoses);
+  connections = bodyPose.getSkeleton();
 }
 
 function selectExercise() {
@@ -30,14 +29,38 @@ function selectExercise() {
 }
 
 function draw() {
-  background(255);
-  image(video, 0, 0);
-  if (segmentation) {
-    image(segmentation.mask, 0, 0, width, height);
+  image(video, 0, 0, width, height);
+
+  for (let i = 0; i < poses.length; i++) {
+    let pose = poses[i];
+    for (let j = 0; j < connections.length; j++) {
+      let pointAIndex = connections[j][0];
+      let pointBIndex = connections[j][1];
+      let pointA = pose.keypoints[pointAIndex];
+      let pointB = pose.keypoints[pointBIndex];
+      // Only draw a line if both points are confident enough
+      if (pointA.score > 0.1 && pointB.score > 0.1) {
+        stroke(255, 0, 0);
+        strokeWeight(2);
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+      }
+    }
+  }
+
+  for (let i = 0; i < poses.length; i++) {
+    let pose = poses[i];
+    for (let j = 0; j < pose.keypoints.length; j++) {
+      let keypoint = pose.keypoints[j];
+      // Only draw a circle if the keypoint's confidence is bigger than 0.1
+      if (keypoint.score > 0.1) {
+        fill(0, 255, 0);
+        noStroke();
+        circle(keypoint.x, keypoint.y, 10);
+      }
+    }
   }
 }
 
-// callback function for body segmentation
-function gotResults(result) {
-  segmentation = result;
+function gotPoses(results) {
+  poses = results;
 }
